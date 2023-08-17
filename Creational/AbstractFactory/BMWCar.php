c811025e124de15a6a62c424b11d11fcce84a9af
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2023-08-17 18:11:38 +0300

    Abstract Factory

---
+++ Creational/AbstractFactory/BMWCar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Creational\AbstractFactory;
+
+class BMWCar implements CarInterface
+{
+
+    private $price;
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+
+    public function calculatePrice()
+    {
+        // TODO: Implement calculatePrice() method.
+
+        return $this->price + 12000;
+    }
+}
